222b94270c0f46a8044e445286b0dae78dfeb929
Sharan Angadi <sharan@mathrusoft.com>
2016-10-27 13:02:33 +0530

    shared pref

--- app/src/main/java/com/mathrusoft/teacher/ActivityLogin.java
+++ app/src/main/java/com/mathrusoft/teacher/ActivityLogin.java
@@ -1,5 +1,8 @@
 package com.mathrusoft.teacher;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -7,47 +10,100 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.mathrusoft.teacher.utils.Constants;
 
 /**
  * Created by sharanangadi on 24/10/16.
  */
 public class ActivityLogin extends AppCompatActivity {
 
+    private static final String TAG = "TA:ActivityLogin";
+
     private EditText mEditTextUsername;
     private EditText mEditTextPassword;
     private Button mButtonSubmit;
+
+//    private TextView mTextViewMessage;
+
+    private Context mContext;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen);
 
+        mContext = this.getApplicationContext();
+
         mEditTextUsername = (EditText) findViewById(R.id.edittext_user_name);
         mEditTextPassword = (EditText) findViewById(R.id.edittext_password);
         mButtonSubmit = (Button) findViewById(R.id.button_submit);
 
+//        mTextViewMessage = (TextView) findViewById(R.id.message);
+//        mTextViewMessage.setVisibility(View.GONE);
+
+
         mButtonSubmit.setOnClickListener(mOnClickListener);
 
 
+        SharedPreferences sharedPreferences = getSharedPreferences(Constants.PREF_NAME, MODE_PRIVATE);
+
+        if (sharedPreferences.getBoolean(Constants.IS_AUTH, false)) {
+            Intent intent = new Intent(ActivityLogin.this, ActivityMain.class);
+            startActivity(intent);
+            ActivityLogin.this.finish();
+        }
     }
 
     View.OnClickListener mOnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
 
-            Log.d("ActivityLog", "Inside OnClick");
+            Log.d(TAG, "Inside OnClick");
 
             String userName = mEditTextUsername.getText().toString();
             String password = mEditTextPassword.getText().toString();
 
-            //make a  server call
 
+            Log.d(TAG, "Inside OnClick");
+
+//            /data/data/<package_name>/shared_prefs/
+            //make a  server call
             if (userName.equalsIgnoreCase(password)) {
-                Toast.makeText(ActivityLogin.this, "User Login Success", Toast.LENGTH_SHORT).show();
+
+                Log.d(TAG, "Inside if statement");
+
+                SharedPreferences sharedPreferences = getSharedPreferences(Constants.PREF_NAME, MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+
+                editor.putString(Constants.USER_NAME, userName);
+                editor.putString(Constants.PASSWORD, password);
+                editor.putBoolean(Constants.IS_AUTH, true);
+
+                editor.commit();
+
+                Intent intent = new Intent(ActivityLogin.this, ActivityMain.class);
+                startActivity(intent);
+                ActivityLogin.this.finish();
+
+                Toast.makeText(mContext, "Welcome...", Toast.LENGTH_SHORT).show();
+//                mTextViewMessage.setText("Welcome...");
+//                mTextViewMessage.setVisibility(View.VISIBLE);
             } else {
-                Toast.makeText(ActivityLogin.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "Inside else statement");
+                Toast.makeText(mContext, "Invalid Credentials.....", Toast.LENGTH_SHORT).show();
+
+//                mTextViewMessage.setText("Invalid Credentials......");
+//                mTextViewMessage.setVisibility(View.VISIBLE);
             }
+
+//            if (userName.equalsIgnoreCase(password)) {
+//                Toast.makeText(ActivityLogin.this, "User Login Success", Toast.LENGTH_SHORT).show();
+//            } else {
+//                Toast.makeText(ActivityLogin.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
+//            }
 
         }
     };
---
+++ app/src/main/java/com/mathrusoft/teacher/utils/Constants.java
@@ -0,0 +1,12 @@
+package com.mathrusoft.teacher.utils;
+
+/**
+ * Created by sharanangadi on 27/10/16.
+ */
+public class Constants {
+    public static final String IS_AUTH = "is_authenticated";
+    public static final String USER_NAME = "user_name";
+    public static final String PASSWORD = "password";
+
+    public static final String PREF_NAME = "my_shared_prefs";
+}
